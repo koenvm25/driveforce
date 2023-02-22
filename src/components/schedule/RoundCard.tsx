c062d0273270b77c5d6fe65e417307eb969050d1
koenvm25 <vanmeggelenkoen@gmail.com>
2023-02-22 23:20:38 +0100

    try fix vercel

--- src/components/schedule/RoundCard.tsx
+++ src/components/schedule/RoundCard.tsx
@@ -1,9 +1,9 @@
 import { Card, Container } from "react-bootstrap";
 import { Race } from "../../domain/calendar";
-import { getCountyCode } from "../../domain/countries";
 import { convertToPeriodString } from "../../domain/dateTimeHelpers";
 import { Result } from "../../domain/result";
 import { TopThree } from "../results/TopThree";
+import { getCountyCode } from "../../domain/country";
 import "./RoundCard.scss";
 
 interface Props {
@@ -21,14 +21,14 @@ export const RoundCard = ({ race, results, onClick }: Props) => {
         <Card.Img
           variant="top"
           src={`https://flagcdn.com/w320/${countryCode?.toLowerCase()}.png`}
-          height='200'
+          height="200"
           alt={race.Circuit.Location.country}
         />
         <Card.Body>
           <Card.Title>{race.raceName}</Card.Title>
           <Card.Text>{race.Circuit.circuitName}</Card.Text>
           <Card.Text>{convertToPeriodString(new Date(race.date))}</Card.Text>
-          {!!results && <TopThree results={results}/>}
+          {!!results && <TopThree results={results} />}
         </Card.Body>
       </Card>
     </Container>
