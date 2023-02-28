884b53f7211c8bd46adc32b347b18840327e0b9a
Koen <vanmeggelenkoen@gmail.com>
2023-02-28 17:01:15 +0100

    add rusults loader

--- src/Components/Results/QualifyingResults.tsx
+++ src/Components/Results/QualifyingResults.tsx
@@ -1,4 +1,14 @@
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
+import {
+  Box,
+  LinearProgress,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
 import { useEffect, useState } from "react";
 import { getQualifyingResults } from "../../Api/endpoints";
 import { QualifyingResult } from "../../Domain/result";
@@ -8,45 +18,55 @@ interface Props {
   round: number;
 }
 
-export const QualifyingResults = ({season, round}: Props) => {
-  const [results, setResults] = useState<QualifyingResult[]>()
+export const QualifyingResults = ({ season, round }: Props) => {
+  const [results, setResults] = useState<QualifyingResult[]>();
 
   useEffect(() => {
     getQualifyingResults(season, round).then((response) => {
       setResults(response.data.MRData.RaceTable?.Races[0]?.QualifyingResults);
-    })
-  }, [season, round])
+    });
+  }, [season, round]);
 
   return (
     <TableContainer>
       {results ? (
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Rank</TableCell>
-              <TableCell>Driver</TableCell>
-              <TableCell>team</TableCell>
-              <TableCell>Q3</TableCell>
-              <TableCell>Q2</TableCell>
-              <TableCell>Q1</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {results.map((result) => (
-              <TableRow>
-                <TableCell>{result.position}</TableCell>
-                <TableCell>{result.Driver.givenName} {result.Driver.familyName}</TableCell>
-                <TableCell>{result.Constructor.name}</TableCell>
-                <TableCell>{result.Q3}</TableCell>
-                <TableCell>{result.Q2}</TableCell>
-                <TableCell>{result.Q1}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      ) : (
+        <>
+          {results.length > 0 ? (
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Rank</TableCell>
+                  <TableCell>Driver</TableCell>
+                  <TableCell>team</TableCell>
+                  <TableCell>Q3</TableCell>
+                  <TableCell>Q2</TableCell>
+                  <TableCell>Q1</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {results.map((result) => (
+                  <TableRow>
+                    <TableCell>{result.position}</TableCell>
+                    <TableCell>
+                      {result.Driver.givenName} {result.Driver.familyName}
+                    </TableCell>
+                    <TableCell>{result.Constructor.name}</TableCell>
+                    <TableCell>{result.Q3}</TableCell>
+                    <TableCell>{result.Q2}</TableCell>
+                    <TableCell>{result.Q1}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          ) : (
             <Typography>Qualifying results are not yet available</Typography>
-        )}
+          )}
+        </>
+      ) : (
+        <Box sx={{ width: "100%" }}>
+          <LinearProgress />
+        </Box>
+      )}
     </TableContainer>
-  )
-}
+  );
+};
--- src/Components/Results/RaceResults.tsx
+++ src/Components/Results/RaceResults.tsx
@@ -1,5 +1,7 @@
 import {
+  Box,
   Divider,
+  LinearProgress,
   Table,
   TableBody,
   TableCell,
@@ -62,74 +64,86 @@ export const RaceResults = ({ season, round, isSprint = false }: Props) => {
     <TableContainer>
       {finished && notFinished ? (
         <>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Rank</TableCell>
-                <TableCell>Driver</TableCell>
-                <TableCell>Team</TableCell>
-                <TableCell>Points</TableCell>
-                <TableCell>Fastes lap</TableCell>
-                <TableCell>Time</TableCell>
-                <TableCell>Laps</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {finished.map((result) => (
-                <TableRow key={result.position}>
-                  <TableCell>{result.position}</TableCell>
-                  <TableCell>
-                    {result.Driver.givenName} {result.Driver.familyName}
-                  </TableCell>
-                  <TableCell>{result.Constructor.name}</TableCell>
-                  <TableCell>{result.points}</TableCell>
-                  <TableCell>
-                    {!!result.FastestLap
-                      ? result.FastestLap.Time?.time
-                      : undefined}
-                  </TableCell>
-                  <TableCell>
-                    {!!result.Time ? result.Time?.time : result.status}
-                  </TableCell>
-                  <TableCell>{result.laps}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-          <Divider sx={{ marginTop: "5px" }} />
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Rank</TableCell>
-                <TableCell>Driver</TableCell>
-                <TableCell>Team</TableCell>
-                <TableCell>Fastes lap</TableCell>
-                <TableCell>Status</TableCell>
-                <TableCell>Reason</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {notFinished.map((result) => (
-                <TableRow key={result.position}>
-                  <TableCell>{result.position}</TableCell>
-                  <TableCell>
-                    {result.Driver.givenName} {result.Driver.familyName}
-                  </TableCell>
-                  <TableCell>{result.Constructor.name}</TableCell>
-                  <TableCell>
-                    {!!result.FastestLap
-                      ? result.FastestLap.Time?.time
-                      : undefined}
-                  </TableCell>
-                  <TableCell>DNF</TableCell>
-                  <TableCell>{result.status}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
+          {finished.length > 0 ? (
+            <>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Rank</TableCell>
+                    <TableCell>Driver</TableCell>
+                    <TableCell>Team</TableCell>
+                    <TableCell>Points</TableCell>
+                    <TableCell>Fastes lap</TableCell>
+                    <TableCell>Time</TableCell>
+                    <TableCell>Laps</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {finished.map((result) => (
+                    <TableRow key={result.position}>
+                      <TableCell>{result.position}</TableCell>
+                      <TableCell>
+                        {result.Driver.givenName} {result.Driver.familyName}
+                      </TableCell>
+                      <TableCell>{result.Constructor.name}</TableCell>
+                      <TableCell>{result.points}</TableCell>
+                      <TableCell>
+                        {!!result.FastestLap
+                          ? result.FastestLap.Time?.time
+                          : undefined}
+                      </TableCell>
+                      <TableCell>
+                        {!!result.Time ? result.Time?.time : result.status}
+                      </TableCell>
+                      <TableCell>{result.laps}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+              <Divider sx={{ marginTop: "5px" }} />
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Rank</TableCell>
+                    <TableCell>Driver</TableCell>
+                    <TableCell>Team</TableCell>
+                    <TableCell>Fastes lap</TableCell>
+                    <TableCell>Status</TableCell>
+                    <TableCell>Reason</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {notFinished.map((result) => (
+                    <TableRow key={result.position}>
+                      <TableCell>{result.position}</TableCell>
+                      <TableCell>
+                        {result.Driver.givenName} {result.Driver.familyName}
+                      </TableCell>
+                      <TableCell>{result.Constructor.name}</TableCell>
+                      <TableCell>
+                        {!!result.FastestLap
+                          ? result.FastestLap.Time?.time
+                          : undefined}
+                      </TableCell>
+                      <TableCell>DNF</TableCell>
+                      <TableCell>{result.status}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </>
+          ) : (
+            <Typography>
+              {isSprint
+                ? "Sprint results are not yet available"
+                : "Race results are not yet available"}
+            </Typography>
+          )}
         </>
       ) : (
-        <Typography>{isSprint ? 'Sprint results are not yet available' : 'Race results are not yet available'}</Typography>
+        <Box sx={{ width: "100%" }}>
+          <LinearProgress />
+        </Box>
       )}
     </TableContainer>
   );
